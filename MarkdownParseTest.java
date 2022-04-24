383149638aecb1ac153ead689d97e903ce534a9e
Angelsofttoy <yiren@ucsd.edu>
2022-04-23 21:39:20 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,20 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.*;
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
+        assertEquals(2, 1 + 1);
+    } 
+
+    @Test
+    public void imageURLTest() throws IOException{
+        Path filename = Path.of()
+        List<String> expectedURLList =  List.of("https://something.com", "https://something.com" )
         assertEquals(2, 1 + 1);
     } 
 }
